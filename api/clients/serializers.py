90f5f9c266840288c8876fe4cac78fa04c6dccb8
XXXANdreyYXXX <andrey.kazantsev.04@inbox.ru>
2024-07-21 11:32:00 +0500

    api

--- api/clients/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import ClientProfile
-
-class ClientSerializer:
-    class Meta:
-        model = ClientProfile
-        fields = '__all__'
-        fields['password'] = 'read_only'
